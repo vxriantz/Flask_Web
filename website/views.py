acf629f580e0508ffcfb0a9135f3cde7f79c28e1
vxriantz <20067@my.sanctamaria.school.nz>
2025-08-10 21:31:13 +1200

    setting up new pages for website

--- website/views.py
+++ website/views.py
@@ -13,18 +13,63 @@ def home():
     return render_template("home.html")
 
 
-# gallery route
-@views.route("/gallery")
+# about route
+@views.route("/about")
 def gallery():
-    return render_template("gallery.html")
+    return render_template("about.html")
 
 
-# contact route
-@views.route("/contact")
+# combat type route
+@views.route("/combat_type")
 def contact():
-    return render_template("contact.html")
+    return render_template("combat_type.html")
 
 # sign up route
 @views.route("/sign_up")
 def sign_up():
     return render_template("sign_up.html")
+
+# faq route
+@views.route("/faq")
+def sign_up():
+    return render_template("faq.html")
+
+# gingerbrave route
+@views.route("/gingerbrave")
+def sign_up():
+    return render_template("gingerbrave.html")
+
+# strawberry cookie route
+@views.route("/strawberry")
+def sign_up():
+    return render_template("strawberry.html")
+
+# wizard cookie route
+@views.route("/wizard")
+def sign_up():
+    return render_template("wizard.html")
+
+# custard cookie iii route
+@views.route("/custard")
+def sign_up():
+    return render_template("custard.html")
+
+# chili pepper cookie route
+@views.route("/chili")
+def sign_up():
+    return render_template("chili.html")
+
+# lime cookie route
+@views.route("/lime")
+def sign_up():
+    return render_template("lime.html")
+
+# jagae cookie route
+@views.route("/jagae")
+def sign_up():
+    return render_template("jagae.html")
+
+# manju cookie route
+@views.route("/manju")
+def sign_up():
+    return render_template("manju.html")
